67f0b7d52211e8738739897f269ba030a0ec26b1
jinugi011 <jinugi011@gmail.com>
2023-04-17 23:32:00 +0900

    First Init

---
+++ BLEScanning/BLEScanning/BLEScanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScanningApp.swift
+//  BLEScanning
+//
+//  Created by Jin Wook Yang on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct BLEScanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
